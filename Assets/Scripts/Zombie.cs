33a139eeab7a7e1ea40a86380d0ccbe653257509
Cristian Laynez <54615310+CrisLayB@users.noreply.github.com>
2024-01-28 13:47:08 -0600

    Stop Condition Fixed

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    private Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if(animator == null)
+            Debug.Log("Error: Animator de zombie no encontrado");
+    }
+
+    private void Update() 
+    {
+        if(transform.position.x < 9f)
+        {
+            animator.SetBool("playStop", true);
+        }
+    }
+
+    // private void OnTriggerEnter(Collider other) 
+    // {
+    //     if(other.tag == "ParedElevador")
+    //     {
+    //         animator.SetBool("playStop", true);
+    //     }
+    // }
+    
+}
