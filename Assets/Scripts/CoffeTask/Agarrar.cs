32c307cb460179b665d52fe44480d3879d755ab3
KennethGalvez <60400757+KennethGalvez@users.noreply.github.com>
2024-01-27 18:16:08 -0600

    Caffe task completa

--- Assets/Scripts/CoffeTask/Agarrar.cs
+++ Assets/Scripts/CoffeTask/Agarrar.cs
@@ -4,40 +4,91 @@ using UnityEngine;
 
 public class Agarrar : MonoBehaviour
 {
-
     public GameObject handPoint;
     private GameObject pickedObject = null;
+    private bool interactingWithMachine = false; 
+    private bool animationPlayed = false; 
 
-
-
-    // Update is called once per frame
     void Update()
     {
-        if (pickedObject!=null)
+        if (pickedObject != null)
         {
+            // Soltar la taza cuando se presiona la tecla "r"
             if (Input.GetKey("r"))
             {
-                pickedObject.GetComponent<Rigidbody>().useGravity = true;
-                pickedObject.GetComponent<Rigidbody>().isKinematic = false;
-                pickedObject.gameObject.transform.SetParent(null);
-                pickedObject = null;
+                DropObject();
             }
-        }
-    }
 
-    private void OnTriggerStay(Collider other) 
-    {
-        if (other.gameObject.CompareTag("CoffeeCup"))
-        {
-            if (Input.GetKey("e") && pickedObject==null)
+            // Llenar la taza "q",
+            if (Input.GetKeyDown("q") && interactingWithMachine)
             {
-                other.GetComponent<Rigidbody>().useGravity = false;
-                other.GetComponent<Rigidbody>().isKinematic = true;
-                other.transform.position = handPoint.transform.position;
-                other.gameObject.transform.SetParent(handPoint.gameObject.transform);
-                pickedObject = other.gameObject;
+                ActivateAnimation();
             }
         }
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        // Recoger la taza al presionar "e" 
+        if (other.gameObject.CompareTag("CoffeeCup") && pickedObject == null)
+        {
+            if (Input.GetKey("e"))
+            {
+                PickUpObject(other.gameObject);
+            }
+        }
+
+        // Verificar si se está interactuando con la máquina de café
+        if (other.gameObject.CompareTag("CoffeeMachine"))
+        {
+            interactingWithMachine = true;
+        }
+
+        // Verificar si la animación del café ya ha ocurrido al tocar el collider
+        if (other.gameObject.CompareTag("WinCollider") && animationPlayed)
+        {
+            Debug.Log("¡Ganaste!");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Dejar de interactuar con la máquina de café cuando el jugador se aleje
+        if (other.gameObject.CompareTag("CoffeeMachine"))
+        {
+            interactingWithMachine = false;
+        }
+    }
+
+    private void PickUpObject(GameObject obj)
+    {
+        obj.GetComponent<Rigidbody>().useGravity = false;
+        obj.GetComponent<Rigidbody>().isKinematic = true;
+        obj.transform.position = handPoint.transform.position;
+        obj.transform.SetParent(handPoint.transform);
+        pickedObject = obj;
+    }
+
+    private void DropObject()
+    {
+        pickedObject.GetComponent<Rigidbody>().useGravity = true;
+        pickedObject.GetComponent<Rigidbody>().isKinematic = false;
+        pickedObject.transform.SetParent(null);
+        pickedObject = null;
+    }
+
+    private void ActivateAnimation()
+    {
+        Animator cupAnimator = pickedObject.GetComponent<Animator>();
+        if (cupAnimator != null)
+        {
+            cupAnimator.SetBool("fillCoffee", true);
+            animationPlayed = true;
+        }
+        else
+        {
+            Debug.Log("No se encontro el animator");
+        }
+    }
 }
+
